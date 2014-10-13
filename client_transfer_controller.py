d871b1989c602ab437deeab58b612a6e91ff694b
Noah Martin <me@noahmart.in>
2014-10-12 21:48:26 -0700

    typo fix

--- client_transfer_controller.py
+++ client_transfer_controller.py
@@ -3,4 +3,4 @@ class ClientTransferController:
   def __init__(self, server_channel, file_src, file_dest, recursive, tcp_mode, disable_verify):
     self.server_channel = server_channel
 
-  derf start(): pass
+  def start(): pass
